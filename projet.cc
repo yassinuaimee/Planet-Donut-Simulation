fb278394c9862e54bc5d4f1affba9e2e3b361589
Adrien Maillet Gonzalez <adrienmailletgonzalez@Adriens-MacBook-Pro.local>
2021-03-18 10:21:05 +0100

    Ajout du switch qui fonctionne bien la normalement. Il va encore falloir ajouter les trucs pour chaque cas spécifiques et les appels de fonctions mais ça avance bien je trouve. La galère du int a été solutionnée par la fonction 'atoi' qui transforme ASCII vers INT

--- projet.cc
+++ projet.cc
@@ -3,57 +3,67 @@ using namespace std;
 
 #include "geomod.h"
 #include "message_geomod.h"
-void verification_nb_arguments(const int,char* argv[] );
+void verification_nb_arguments(const int,char**);
 
 
 int main(int argc,char* argv[] )
 {
-//verification_nb_arguments(argc,argv[]);
+	cout<<"ARGC : "<<argc<<endl;
+	verification_nb_arguments(argc,argv);
 
-cout<<argc<<endl;
-cout<<argv[0]<<endl;
 }
 
 
 
-void verification_nb_arguments(const int n, char* argv[])
+void verification_nb_arguments(const int argc, char* argv[])
 {
-	switch(*(reinterpret_cast<int*>(argv[1] ))){
-		
-		case(1): if(n-2!=1)
+	int i(atoi(argv[1] )); //Permet la conversion de chaine de caractere ASCII vers entier
+	cout<<i<<endl;
+	switch(i){
+		case(1): if(argc-2!=1)
 						{
 							message_geomod::bad_argc();
 							exit(0);
 						}
-		case(2):if(n-2!=3)
+					else
+					{
+						cout<<argv[1]<<endl;
+					}
+					break;
+		case(2):if(argc-2!=3)
 						{
 							message_geomod::bad_argc();
 							exit(0);
 						}
-			
-		case(3):if(n-2!=5)
+					break;
+		case(3):if(argc-2!=5)
 						{
 							message_geomod::bad_argc();
 							exit(0);
 						}
-		case(4):if(n-2!=5)
+					break;
+		case(4):if(argc-2!=5)
 						{
 							message_geomod::bad_argc();
 							exit(0);
 						}
-		case(5):if(n-2!=6)
+					break;
+		case(5):if(argc-2!=6)
 						{
 							message_geomod::bad_argc();
 							exit(0);
 						}
-						
-		case(6):if(n-2!=7)
+					break;	
+		case(6):if(argc-2!=7)
 						{
 							message_geomod::bad_argc();
 							exit(0);
 						}
+					break;
 		default: 
-				message_geomod::bad_argc();
-				exit(0);
+					cout<<"Default"<<endl;
+					message_geomod::bad_argc();
+					exit(0);
+					break;
 }
 }
